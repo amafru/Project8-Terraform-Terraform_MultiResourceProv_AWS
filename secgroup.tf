cc97e77203373a70fd6e11783f5fc4aecce0cc8a
amafru <fruamabuh@gmail.com>
2023-08-03 14:40:13 +0100

    modified secgroup.tf

--- secgroup.tf
+++ secgroup.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "project8-stack-sg" {
     cidr_blocks = [var.MYIP]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.MYIP]
+  }
+
   tags = {
     Name = "project8-sg"
   }
