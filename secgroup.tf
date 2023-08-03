831d21adf1881d062b7e8629c736a56ed81c7a63
amafru <fruamabuh@gmail.com>
2023-08-03 13:49:29 +0100

    added a gitignore and spec'd secgroup.tf

---
+++ secgroup.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "project8-stack-sg" {
+  vpc_id      = aws_vpc.project8-vpc.id
+  name = "proj-8-sg"
+  description = "project8 SG. Allow ssh from my IP"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.MYIP]
+  }
+
+  tags = {
+    Name = "project8-sg"
+  }
+}
